d29f7a98a6a352a4f550cd8b9eb1717bbd37f59b
nohtrim <nohtrim@gmail.com>
2015-12-18 12:31:54 +0000

    4562

--- app.js
+++ app.js
@@ -17,8 +17,11 @@ app.use(require('./controllers'))
 })*/
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '0.0.0.0'
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '127.0.0.1'
 
 app.listen(server_port, server_ip_address, function(){
   console.log("Listening on " + server_ip_address + ", server_port " + server_port)
 });
+
+//http://senserover-terrestre.rhcloud.com/
+//dw32igsr@gmail.com
