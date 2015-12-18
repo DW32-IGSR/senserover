e1740fb98e3e8970e84df807cddf21e3d0e0bfa1
nohtrim <nohtrim@gmail.com>
2015-12-18 07:19:22 +0000

    4545

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(require('./controllers'))
 
-app.listen(port, function() {
+/*app.listen(port, function() {
   console.log('Listening on port ' + port)
-})
+})*/
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '127.0.0.1'
+
+app.listen(server_port, server_ip_address, function(){
+  console.log("Listening on " + server_ip_address + ", server_port " + server_port)
+});
