f72e16c1b7af26fa3dd08b6203e030c0cb0e4c96
nohtrim <nohtrim@gmail.com>
2015-12-18 12:29:14 +0000

    4562

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 var express = require('express')
   , router = express.Router()
-var mongoose = require('mongoose');  
+var mongoose = require('mongoose')
+
+var Dato = require('../models/Dato')
 
 router.use('/comments', require('./comments'))
 router.use('/users', require('./users'))
@@ -12,7 +14,24 @@ router.get('/', function(req, res) {
   db.once('open', function callback() {
     console.log('db connection open')
   })  
+  Dato.find({}, function (err, dato) {
+    if (err){
+      console.log('error occured in the database')
+    }
+      //console.log(dato)
+      console.log("prueba humedad: "+dato[0].humedad)
+  })
+  /*
+    var res = null;
+    Ninja.find({},'name skill',function(err,docs){
+        if (err)
+            console.log('error occured in the database');
+        console.log(docs);
+    });     
+    return res;  
+  */
   res.render('index')
+  //res.render('administracion')
 })
 
 module.exports = router
---
+++ models/Dato.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var mongoose = require('mongoose');
+//var bcrypt = require('bcrypt');
+
+var datoModel = function () {
+    
+    var datoSchema = new mongoose.Schema({
+        temperatura: String,
+        humedad: String,
+        co2: String,
+        radiacion: String,
+        luminosidad: String,
+        fecha: String
+    },{collection : 'datos'});
+    
+    /*
+    {
+        "_id": {
+            "$oid": "566b17e8bc1973d60d8b4567"
+        },
+        "temperatura": "21.5",
+        "humedad": "2",
+        "co2": "5",
+        "radiacion": "2",
+        "luminosidad": "16",
+        "fecha": "2015-12-11 18:37:28"
+    }
+    */
+
+    /*userSchema.methods.comparePassword = function(candidatePassword, cb) {
+        bcrypt.compare(candidatePassword, this.pass, function(err, isMatch) {
+            if (err) return cb(err);
+            cb(null, isMatch);
+        });
+    };  
+    
+    datoSchema.methods.activarUsuario = function () {
+        if(this.validated){
+            //no hacer nada
+            return 'esta validado';
+        }else{
+            //update de usuario no validado
+            return 'no esta validado';
+        }
+    };
+    
+    datoSchema.methods.comparePassword = function(candidatePassword, cb) {
+        bcrypt.compare(candidatePassword, this.pass, function(err, isMatch) {
+            if (err) return cb(err);
+            cb(null, isMatch);
+        });
+    };*/  
+    
+    return mongoose.model('Dato', datoSchema);
+};
+
+module.exports = new datoModel();
