13667203820ae47cbd87cd601f5692d97122361f
nohtrim <nohtrim@gmail.com>
2016-01-15 12:40:46 +0000

    viernes

--- controllers/RangoFecha.js
+++ controllers/RangoFecha.js
@@ -13,7 +13,7 @@ exports.rangoFecha = function(req, res) {
     console.log('Fecha final: ' + fecha_final) //2016-01-08
     
     //Dato.find({id_dron : id_dron, fecha: {$ne: [fecha_inicio, fecha_final]}}, function (err, dato) {
-    Dato.find({id_dron : id_dron, fecha : {$lte : fecha_final, $gte : fecha_inicio}}, function (err, dato) {
+    Dato.find({id_dron : id_dron, fecha : {$lte : '^'+fecha_final, $gte : fecha_inicio}}, function (err, dato) {
         if (err){
             console.log('prueba ruben: error occured in the database')
         }
--- public/js/botonesSession.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $("#prueba1").on("click", function(){ 
-        //alert("fdgdfgdfgdf");
-        $('#registro').tabs();
-    });
-});
