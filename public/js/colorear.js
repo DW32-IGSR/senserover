0caa5a9f369553bb753a370595dbbdb25bddfeec
nohtrim <nohtrim@gmail.com>
2016-01-23 10:00:09 +0000

    colorear 2.0

--- public/js/colorear.js
+++ public/js/colorear.js
@@ -1,73 +1,61 @@
 function colorearEstado(){
     // verde=bg-green amarillo=bg-orange rojo=bg-red
-    // http://www.desarrolloweb.com/articulos/anadir-quitar-class-jquery.html
     // estado_tem estado_hum estado_co2 estado_rad estado_lum estado_bat
+    // http://www.desarrolloweb.com/articulos/anadir-quitar-class-jquery.html
     console.log("coloreando :D")
     
-    function quitarClases(id){
+    function colorear(id,color){
         $(id).removeClass("bg-green")
         $(id).removeClass("bg-red")
         $(id).removeClass("bg-orange")
+        $(id).addClass(color)
     }
     
     $.ajax({
         type: "GET",
-        //url: "http://dron-terrestre.rhcloud.com/datosj.php",
+        //http://senserover-terrestre.rhcloud.com/alertas/rango/56939648e4b0166e3b6a60f6
         //http://sense-rover-nohtrim.c9users.io/alertas/rango/56939648e4b0166e3b6a60f6
         url: "http://sense-rover-nohtrim.c9users.io/alertas/rango/"+$("#dron_seleccionado").html(),
         dataType: "json",
         success: function(data) {
-            console.log("exito buscar rango")
+            console.log("exito buscar rangos")
             if(datosTemp[datosTemp.length-1]>data[0].temperatura.max||datosTemp[datosTemp.length-1]<data[0].temperatura.min){
-                quitarClases("#estado_tem")
-                $("#estado_tem").addClass("bg-red")
+                colorear("#estado_tem","bg-red")
             }else{
-                quitarClases("#estado_tem")
-                $("#estado_tem").addClass("bg-green")
+                colorear("#estado_tem","bg-green")
             }
             
             if(datosHum[datosHum.length-1]>data[0].humedad.max||datosHum[datosHum.length-1]<data[0].humedad.min){
-                quitarClases("#estado_hum")
-                $("#estado_hum").addClass("bg-red")
+                colorear("#estado_hum","bg-red")
             }else{
-                quitarClases("#estado_hum")
-                $("#estado_hum").addClass("bg-green")
+                colorear("#estado_hum","bg-green")
             }
             
-            if(datosCO2[datosCO2.length-1]>data[0].co2.max || datosCO2[datosCO2.length-1]<data[0].co2.min){
-                quitarClases("#estado_co2")
-                $("#estado_co2").addClass("bg-red")
+            if(datosCO2[datosCO2.length-1]>data[0].co2.max||datosCO2[datosCO2.length-1]<data[0].co2.min){
+                colorear("#estado_co2","bg-red")
             }else{
-                quitarClases("#estado_co2")
-                $("#estado_co2").addClass("bg-green")
+                colorear("#estado_co2","bg-green")
             }
             
-            if(datosRad[datosRad.length-1]>data[0].radiacion.max || datosRad[datosRad.length-1]<data[0].radiacion.min ){
-                quitarClases("#estado_rad")
-                $("#estado_rad").addClass("bg-red")
+            if(datosRad[datosRad.length-1]>data[0].radiacion.max||datosRad[datosRad.length-1]<data[0].radiacion.min ){
+                colorear("#estado_rad","bg-red")
             }else{
-                quitarClases("#estado_rad")
-                $("#estado_rad").addClass("bg-green")
+                colorear("#estado_rad","bg-green")
             }
             
-            if(datosLum[datosLum.length-1] > data[0].luminosidad.max || datosLum[datosLum.length-1] < data[0].luminosidad.min){
-                quitarClases("#estado_lum")
-                $("#estado_lum").addClass("bg-red")
+            if(datosLum[datosLum.length-1]>data[0].luminosidad.max||datosLum[datosLum.length-1] < data[0].luminosidad.min){
+                colorear("#estado_lum","bg-red")
             }else{
-                quitarClases("#estado_lum")
-                $("#estado_lum").addClass("bg-green")
+                colorear("#estado_lum","bg-green")
             }
             
-            if(datosBat[datosBat.length-1] < data[0].bateria.min){
-                quitarClases("#estado_bat")
-                $("#estado_bat").addClass("bg-red")
+            if(datosBat[datosBat.length-1]<data[0].bateria.min){
+                colorear("#estado_bat","bg-red")
             }else{
                 if(datosBat[datosBat.length-1]<10){
-                    quitarClases("#estado_bat")
-                    $("#estado_bat").addClass("bg-orange")
+                    colorear("#estado_bat","bg-orange")
                 }else{
-                    quitarClases("#estado_bat")
-                    $("#estado_bat").addClass("bg-green")
+                    colorear("#estado_bat","bg-green")
                 }
             }
         },
