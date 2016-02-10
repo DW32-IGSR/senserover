bac633e7f174549052391e8f6eeeb7ed9dddc82b
nohtrim <nohtrim@gmail.com>
2016-02-10 07:45:07 +0000

    miercoles

--- controllers/Perfil.js
+++ controllers/Perfil.js
@@ -6,13 +6,14 @@ var moment = require('moment');
 
 exports.perfil = function(req, res) {
     //ruta a la pagina de perfil
-    
+
     if (req.user == "" || req.user == undefined) {
         //console.log('Cumplo la primera condición ');
-        if(req.session == "" || req.session == undefined){
+        if (req.session == "" || req.session == undefined) {
             //console.log('Cumplo la segunda condición ');
             res.render('/');
-        }else {
+        }
+        else {
             //console.log('Entro return 1 ');
             var sess = req.session;
 
@@ -30,47 +31,54 @@ exports.perfil = function(req, res) {
                             console.log(err);
                         }
                         else {
-                            
-                            Drones.count({id_usuario: sess.id_usuario}, function(err, dronesCant) {
-                               console.log('Tiene ' + dronesCant + ' Drones');
-                               
-                               var arrays = {
+
+                            Drones.count({
+                                id_usuario: sess.id_usuario
+                            }, function(err, dronesCant) {
+                                if (err) {
+                                    console.log(err);
+                                }
+                                console.log('Tiene ' + dronesCant + ' Drones');
+
+                                var arrays = {
                                     drones_perfil: drones_encontrados,
                                     datos_perfil: datos_usuario,
                                     nombre_usuario: sess.usuario,
                                 };
-                               var prueba = [];
-                               for ( var i = 0; i < dronesCant; i++) {
-                                   
+                                var prueba = [];
+                                for (var i = 0; i < dronesCant; i++) {
+
                                     var a = moment().format("Y-MM-DD");
                                     var b = moment(drones_encontrados[i].fecha_caducidad, 'Y-MM-DD');
                                     var days = b.diff(a, 'days');
-                                    
+
                                     console.log('Fecha ' + i + 'Dias que faltan: ' + days);
-                                    
-                                    prueba.push({days});
-                                    
+
+                                    prueba.push({
+                                        days
+                                    });
+
                                     //console.log('Resultado array ' + diasRestantes.days);
                                 }
-                                
+
                                 /*var prueba = [
                                     {variable:"contenido 1"},
                                     {variable:"contenido 2"},
                                     {variable:"contenido 3"}
                                 ];*/
-                                                            
-                                
+
+
                                 //console.log(arrays);
                                 //var arrays = {array_perfil, array_perfil_datos} // No funciona
                                 res.render('perfil', arrays);
-                                
-                                
+
+
                             });
-                            
-                           /* var fechaActual = moment().format("Y-MM-DD");
-                            var fechaCaducidad = moment(drones_encontrados.fecha_caducidad);
-                            fechaActual.diff(fechaCaducidad, 'days');*/
-                            
+
+                            /* var fechaActual = moment().format("Y-MM-DD");
+                             var fechaCaducidad = moment(drones_encontrados.fecha_caducidad);
+                             fechaActual.diff(fechaCaducidad, 'days');*/
+
                             /*
                             var hoy = moment(new Date());//todays date
                             var final = moment("2016-06-1"); // another date
@@ -84,7 +92,7 @@ exports.perfil = function(req, res) {
                                 var days = duration.asDays();
                                 console.log(days)
                             */
-                            
+
                             /*
                                 // Funciona
                                 var a = moment('2016-02-04', 'Y-MM-DD');
@@ -93,25 +101,26 @@ exports.perfil = function(req, res) {
                                 
                                 console.log('ruben fecha: ' + days);
                             */
-                            
+
                             /* EL QUE FUNCIONA
                             var a = moment().format("Y-MM-DD");
                             var b = moment('2016-02-07', 'Y-MM-DD');
                             var days = b.diff(a, 'days');
                             
                             console.log('Dias que faltan: ' + days); */
-    
+
                         }
                     });
                 }
             });
         }
 
-    } else {
+    }
+    else {
         //console.log('Entro return 2 ');
         var sess = req.user;
         console.log(sess.id);
-        
+
         Usuario.findOne({
             _id: sess.id
         }, function(err, datos_usuario) {
@@ -126,32 +135,36 @@ exports.perfil = function(req, res) {
                         console.log(err);
                     }
                     else {
-                        
-                        Drones.count({id_usuario: sess.id}, function(err, dronesCant) {
-                           console.log('Tiene ' + dronesCant + ' Drones');
-                           
-                           var arrays = {
+
+                        Drones.count({
+                            id_usuario: sess.id
+                        }, function(err, dronesCant) {
+                            console.log('Tiene ' + dronesCant + ' Drones');
+
+                            var arrays = {
                                 drones_perfil: drones_encontrados,
                                 datos_perfil: datos_usuario,
                                 nombre_usuarioGoogle: sess.profile.name,
                                 avatarGoogle: sess.profile.picture
                             };
                             console.log(arrays);
-                           var prueba = [];
-                           for ( var i = 0; i < dronesCant; i++) {
-                               
+                            var prueba = [];
+                            for (var i = 0; i < dronesCant; i++) {
+
                                 var a = moment().format("Y-MM-DD");
                                 var b = moment(drones_encontrados[i].fecha_caducidad, 'Y-MM-DD');
                                 var days = b.diff(a, 'days');
-                                
+
                                 console.log('Fecha ' + i + 'Dias que faltan: ' + days);
-                                
-                                prueba.push({days});
-                                
+
+                                prueba.push({
+                                    days
+                                });
+
                                 //console.log('Resultado array ' + diasRestantes.days);
                             }
                             res.render('perfil', arrays);
-             
+
                         });
                     }
                 });
@@ -171,27 +184,28 @@ exports.datosPerfil = function(req, res) {
         if (req.user == "" || req.user == undefined) {
             console.log('Cumplo la segunda condición ');
             res.redirect('/');
-        } else {
+        }
+        else {
             console.log('Entro en UPDATE ');
             var sess = req.user;
             console.log(sess.id);
             console.log(sess.nombre);
-            
-            
+
+
             var form_nombre = req.body.nombre;
             var form_apellidos = req.body.apellidos;
             var form_dni = req.body.dni;
             var form_direccion = req.body.direccion;
             var form_cp = req.body.cp;
             var form_email = req.body.email;
-    
+
             console.log('Nombre: ' + form_nombre);
             console.log('Apellidos: ' + form_apellidos);
             console.log('DNI: ' + form_dni);
             console.log('Dirección: ' + form_direccion);
             console.log('Código Postal: ' + form_cp);
             console.log('Email: ' + form_email);
-    
+
             // Validacion servidor
             req.assert('nombre', 'El nombre es requerido.').notEmpty();
             req.assert('nombre', 'Nombre usa al menos 3 a 20 caracteres.').len(3, 20);
@@ -206,11 +220,11 @@ exports.datosPerfil = function(req, res) {
             req.assert('cp', 'El CP tiene que ser numerico.').isInt();
             req.assert('email', 'El email es requerido.').notEmpty();
             req.assert('email', 'Email no valido.').isEmail();
-    
+
             var errors = req.validationErrors();
-    
+
             console.log(errors);
-    
+
             if (errors) {
                 return res.redirect('/');
             }
@@ -237,9 +251,10 @@ exports.datosPerfil = function(req, res) {
                         res.redirect('/perfil');
                     } //else error
                 }); //find update
-            } 
+            }
         }
-    } else { //si no redireccion a pagina de inicio
+    }
+    else { //si no redireccion a pagina de inicio
 
         var form_nombre = req.body.nombre;
         var form_apellidos = req.body.apellidos;
